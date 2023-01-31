9f58aca9c0b82c18f3a945953829f6c35438a923
Mansur <vakhabov.mans@gmail.com>
2023-01-31 23:50:00 +0300

    настроил структуру sass-файлов, начал адаптив

---
+++ src/components/Header-component.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <div class="container header_container">
+      <div class="logo">
+        <img src="../assets/logo1.svg" alt="logo" />
+      </div>
+      <nav>
+        <ul class="linksWraper">
+          <li class="link"><a href="#">Главное</a></li>
+          <li class="link"><a href="#">О нас</a></li>
+          <li class="link"><a href="#">Обратная связь</a></li>
+          <li class="link"><a href="#">Сообщество</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
